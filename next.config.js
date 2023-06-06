ff90fcb15bf2d2f566e269bc42a2ded3a25933dc
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-06-06 18:02:30 +0300

    Set up the next js environment with styled-components

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    productionBrowserSourceMaps: true,
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.amazonaws.com',
+                port: '',
+                pathname: '/my-bucket/**',
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
