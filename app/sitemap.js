ef776a4d42b281870d3cccbfafc53c28d586e819
Maksym <90103151+MaksVibe@users.noreply.github.com>
2023-06-12 16:25:16 +0300

    fix: production features

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,19 +1,7 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://site.commmmm',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://site.commmmm/about',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://site.commmmm/profile',
-            lastModified: new Date(),
-        },
-        {
-            url: 'https://site.commmmm/contacts',
+            url: 'https://mp-o.vercel.app/',
             lastModified: new Date(),
         },
     ];
